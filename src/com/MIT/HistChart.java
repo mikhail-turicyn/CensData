67531aacf8b184b937de0b9918ce31cca446ef48
qq <mikhail-turicyn@yandex.ru>
2018-07-26 07:21:38 +0300

    end

---
+++ src/com/MIT/HistChart.java
@@ -0,0 +1,35 @@
+package com.MIT;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberTickUnit;
+import org.jfree.chart.plot.PlotOrientation;
+
+class HistChart extends Chart {
+    public HistChart(String appTitle,
+                     double[] data,
+                     int spitCnt,
+                     String chartTitle) {
+        this(appTitle, data, spitCnt, chartTitle, "", "", null);
+    }
+
+    HistChart(String appTitle,
+              double[] data,
+              int spitCnt,
+              String chartTitle,
+              String xAxisLabel,
+              String yAxisLabel,
+              NumberTickUnit tickUnit) {
+        super(appTitle);
+        JFreeChart chart = ChartFactory.
+                createHistogram(chartTitle, xAxisLabel, yAxisLabel,
+                        createDataset(data, spitCnt),
+                        PlotOrientation.VERTICAL, false, false, false);
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(560, 367));
+        setContentPane(chartPanel);
+        saveAsFile(chart);
+    }
+}
--- src/com/MIT/Main.java
+++ src/com/MIT/Main.java
@@ -3,11 +3,11 @@ package com.MIT;
 import java.util.ArrayList;
 
 public class Main {
-    private static final double lambda = 0.001;
-    private static int dataCount = 50000;
-    private static int censCount = 1000;
+    private static final double lambda = 0.003;
+    private static int dataCount = 100;
+    private static int censCount = 10;
 
-    private static ArrayList data;
+    private static ArrayList<Double> data;
     private static ArrayList dataCens;
 
 
@@ -15,16 +15,27 @@ public class Main {
         // write your code here
         double res = 0;
         CensCalc cc = new CensCalc();
+//        data = cc.generateExpData(1000, lambda);
+//
+//        double d[] = new double[data.size()];
+//        for (int i = 0; i < data.size(); i++) {
+//            d[i]=data.get(i);
+//        }
+//
+//        HistChart histChart = new HistChart("Распределение значений", d, 20, "Распределение значений", "x", "P(x)", null);
+//        histChart.pack();
+//        histChart.setVisible(true);
         for (int i = 0; i < 5; i++) {
             data = cc.generateExpData(dataCount, lambda);
             dataCens = cc.generateExpData(censCount, lambda);
+
 //            res = cc.find(0,1, 0.000001, data,dataCens );
             res = cc.rightCens(data, dataCens);
             System.out.print("T " + censCount + "labda is" + res + "\t");
 //            res = cc.accuracyLeft(res,(double)censCount, dataCens);
             res = cc.accuracyRight(res, dataCount);
-            System.out.println("accuracy is" + res + "\t");
-            censCount += 500;
+            System.out.println("accuracy is" + Math.sqrt(res) + "\t");
+            censCount += 10;
         }
 
 //        res = cc.rightCens(data,dataCens );
